591e85b8af2bbd19dd839bef7c8aa29f612b2f51
Hugo <wheredidhugo@protonmail.com>
2022-06-10 23:14:23 +0200

    fix: footer border now rounded and better!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
-    <div className="border-t-2 border-white/[.15] text-center py-5">
+    <div className="px-7 mx-auto text-center">
+      <div className="bg-white opacity-20 w-auto border-2 border-white rounded-[25px] my-5"></div>
       <a href="https://github.com/2202x2/freenom">
         <FontAwesomeIcon
           className="text-5xl opacity-20 hover:opacity-100"
